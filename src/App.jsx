c412b2748b90b64cbf1c4d71eca410063dfd4c59
igudy <goodnessigunma1@gmail.com>
2023-10-20 00:45:58 +0100

    feat: assets added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Hero } from "./components";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
+
+export default App;
