9a93ccd6e0f5002be4802a706445acb728c2b963
igudy <goodnessigunma1@gmail.com>
2023-10-26 21:01:41 +0100

    feat: stories slider

--- src/components/TopStories.jsx
+++ src/components/TopStories.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { story } from "../data/data";
-import SwiperCore, { Navigation, Pagination } from "swiper";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper-bundle.css";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import StoriesCard from "./sub-components/StoriesCard";
 
 const TopStories = () => {
   return (
@@ -13,46 +19,50 @@ const TopStories = () => {
       <div>
         <Swiper
           breakpoints={{
-            // when window width is >= 375px
             375: {
               width: 375,
               slidesPerView: 2,
+              spaceBetween: 30,
             },
             550: {
               width: 550,
               slidesPerView: 2,
+              spaceBetween: 30,
             },
             767: {
               width: 767,
               slidesPerView: 3,
+              spaceBetween: 30,
             },
             991: {
               width: 991,
-              slidesPerView: 3,
+              slidesPerView: 5,
+              spaceBetween: 30,
             },
             1200: {
               width: 1200,
-              slidesPerView: 3,
+              slidesPerView: 5,
+              spaceBetween: 30,
             },
           }}
           navigation
           pagination={{ clickable: true }}
+          scrollbar={{ draggable: true, dragSize: "20px" }}
         >
-          {/* {slides.map((item) => (
-        <SwiperSlide key={item.id}>
-          <ProductCards
-            title={item.title}
-            text={item.text}
-            rating={item.rating}
-            btn={item.btn}
-            img={item.img}
-            price={item.price}
-            color={item.color}
-            shadow={item.shadow}
-          />
-        </SwiperSlide>
-      ))} */}
-          {/* Add news here */}
+          {story?.news?.map((item, i) => (
+            <SwiperSlide key={i} className="flex cursor-pointer">
+              <StoriesCard
+                title={item.title}
+                text={item.text}
+                img={item.img}
+                url={item.url}
+                like={item.like}
+                time={item.time}
+                by={item.by}
+                btn={item.btn}
+              />
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </>
--- src/components/sub-components/StoriesCard.jsx
+++ src/components/sub-components/StoriesCard.jsx
@@ -1,25 +1,33 @@
-import React from "react";
+import truncate from "lodash/truncate";
 
 const StoriesCard = ({ title, text, img, url, like, time, by, btn }) => {
+  const truncatedText = truncate(text, {
+    length: 100,
+    omission: "...",
+  });
   return (
     <>
-      <div className="overflow-hidden h-40 w-32 drop-shadow-xl text-white">
+      <div className="mx-6 h-[450px] w-[300px] rounded-2xl shadow-2xl text-black">
         <div>
-          <img src={img} className={title} />
+          <img src={img} className="rounded-t-xl" alt={title} />
         </div>
-        <div className="flex justify-between gap-5">
-          <div className="left-0">{like}</div>
-          <div className="">{time}</div>
-          <div className="right-0 text-blue-700">#{by}</div>
-        </div>
-        <div>
-          <p>{title}</p>
-          <p>{text}</p>
-        </div>
-        <div className="w-full bg-black rounded-xl h-12">
-          <a href={url}>
-            <p className="text-white">{btn}</p>
-          </a>
+        <div className="px-4">
+          <div className="flex justify-between gap-5 my-3">
+            <div className="left-0 text-sm">{like}</div>
+            <div className="text-sm">{time}</div>
+            <div className="right-0 font-bold text-blue-700 text-sm">#{by}</div>
+          </div>
+          <div>
+            <p>{title}</p>
+            <p className="text-sm">{truncatedText}</p>
+          </div>
+          <div className="flex text-center justify-center w-full my-5 bg-black rounded-xl h-10">
+            <a href={url}>
+              <p className="text-white text-center justify-center items-center">
+                {btn}
+              </p>
+            </a>
+          </div>
         </div>
       </div>
     </>
