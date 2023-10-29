39737d168f96379b5cb46934b82c171970ba80a4
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-10-29 16:16:00 +0100

    feat: CartModal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import logo from "../assets/logo.png";
 import CartModal from "./sub-components/cart/CartModal";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   const closeModal = () => {
     setIsOpen(false);
@@ -21,14 +21,18 @@ const Navbar = () => {
   return (
     <div className="flex flex-row relative justify-between items-center mx-14 py-8 xsm:mx-2 xsm:py-6 sm:mx-2 sm:py-8 md:mx-8 md:py-8">
       <div className="">
-        <img src={logo} className="w-20 h-6" alt="logo" />
+        <img src={logo} className="w-20 h-6 cursor-pointer" alt="logo" />
       </div>
       <div className="flex text-white items-center gap-4 xsm:gap-0 sm:gap-1 right-0">
-        <HeartIcon className="w-8 h-6" />
+        <HeartIcon className="w-8 h-6 cursor-pointer" />
 
-        <MagnifyingGlassIcon className="w-8 h-6" />
+        <MagnifyingGlassIcon className="w-8 h-6 cursor-pointer" />
 
-        <CartModal openModal={openModal} />
+        <CartModal
+          openModal={openModal}
+          closeModal={closeModal}
+          isOpen={isOpen}
+        />
       </div>
     </div>
   );
--- src/components/sub-components/cart/CartModal.jsx
+++ src/components/sub-components/cart/CartModal.jsx
@@ -1,58 +1,80 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
 import { ShoppingBagIcon } from "@heroicons/react/24/outline";
+import { IoArrowBackOutline } from "react-icons/io5";
+import { AiFillCloseCircle, AiFillCloseSquare } from "react-icons/ai";
 
-const CartModal = ({ openModal, cl }) => {
+const CartModal = ({ openModal, closeModal, isOpen }) => {
   return (
     <>
-      <ShoppingBagIcon type="button" onClick={openModal} className="w-8 h-6" />
+      <ShoppingBagIcon
+        type="button"
+        onClick={openModal}
+        className="w-8 h-6 cursor-pointer"
+      />
+
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Dialog as="div" className="relative z-[999]" onClose={closeModal}>
           <Transition.Child
             as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+            enter="transform transition-transform ease-out duration-600"
+            enterFrom="translate-x-[100%] opacity-0"
+            enterTo="translate-x-0 opacity-100"
+            leave="transform transition-transform ease-in duration-100"
+            leaveFrom="translate-x-0 opacity-100"
+            leaveTo="translate-x-[100%] opacity-0"
           >
-            <div className="fixed inset-0 bg-black/25" />
+            <div className="fixed inset-0 opacity-80 bg-slate-100" />
           </Transition.Child>
 
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <div className="fixed top-0 right-0 xsm:w-[100%] sm:w-[100%] md:w-[100%] w-[50%]">
+            <div className="">
               <Transition.Child
                 as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
+                enter="transform transition-transform ease-out duration-300"
+                enterFrom="translate-x-[100%] opacity-0"
+                enterTo="translate-x-0 opacity-100"
+                leave="transform transition-transform ease-in duration-100"
+                leaveFrom="translate-x-0 opacity-100"
+                leaveTo="translate-x-[100%] opacity-0"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
-                  >
-                    Payment successful
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      Your payment has been successfully submitted. We’ve sent
-                      you an email with all of the details of your order.
-                    </p>
-                  </div>
-
-                  <div className="mt-4">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={closeModal}
-                    >
-                      Got it, thanks!
-                    </button>
+                <Dialog.Panel className="w-full transform overflow-hidden h-screen bg-white text-left align-middle shadow-xl transition-all">
+                  <div className="mt-2 mx-4">
+                    <p className="flex justify-between text-md">
+                      <div className="flex left-0 items-center">
+                        <IoArrowBackOutline
+                          className="w-6 h-6 cursor-pointer"
+                          onClick={closeModal}
+                        />{" "}
+                        <span className="ml-2">
+                          Your cart
+                          <span className="bg-black mx-2 rounded-lg text-white">
+                            <span className="px-3 py-3">1 item(s)</span>
+                          </span>
+                        </span>
+                      </div>
+                      <div className="right-0">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-10 h-10 hover:text-slate-300 cursor-pointer"
+                          onClick={closeModal}
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                          />
+                        </svg>
+                      </div>
+                    </p>{" "}
+                    {/* Cart Section */}
+                    <div className="">
+                      <p>Hello</p>
+                    </div>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
